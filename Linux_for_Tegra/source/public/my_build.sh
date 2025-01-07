734df8dfb09cfb64d9a5386f9cfdcf2b2c115690
chenjing <chenjing@intron-tech.com>
2025-01-07 16:10:36 +0800

    add camera irs2877a support
    Signed-off-by: chenjing <chenjing@intron-tech.com>

---
+++ Linux_for_Tegra/source/public/my_build.sh
@@ -0,0 +1,11 @@
+
+export CROSS_COMPILE_AARCH64_PATH=/home/user/work/git/orin_test/toolchain
+export CROSS_COMPILE_AARCH64=/home/user/work/git/orin_test/toolchain/bin/aarch64-buildroot-linux-gnu-
+
+mkdir -p kernel_out
+./nvbuild.sh -o $PWD/kernel_out
+
+mkdir -p my_out
+cp ./kernel_out/arch/arm64/boot/Image  ./my_out/Image_test
+cp ./kernel_out/arch/arm64/boot/dts/nvidia/tegra234-p3701-0004-p3737-0000.dtb ./my_out/tegra234-p3701-0004-p3737-0000_test.dtb
+cp ./kernel_out/drivers/media/i2c/nv_irs2877a.ko  ./my_out/nv_irs2877a.ko
